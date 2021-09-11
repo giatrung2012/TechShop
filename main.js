125c154b5492d171218ca129bd1cd80ff9f7d303
giatrung2012 <trungrappar2002@gmail.com>
2021-09-11 13:17:29 +0700

    Change somethings

--- main.js
+++ main.js
@@ -1,52 +1,51 @@
 // Modal layout
-var modal = document.querySelector(".modal");
-var close = document.querySelector(".modal__overlay");
+var modal = document.querySelector('.modal');
+var close = document.querySelector('.modal__overlay');
 
-var signUpBtn = document.getElementById("sign-up-btn");
-var logInBtn = document.getElementById("log-in-btn");
+var signUpBtn = document.getElementById('sign-up-btn');
+var logInBtn = document.getElementById('log-in-btn');
 
-var switchToSignUpBtn = document.getElementById("switch-to-sign-up-btn");
-var switchToLogInBtn = document.getElementById("switch-to-log-in-btn");
+var switchToSignUpBtn = document.getElementById('switch-to-sign-up-btn');
+var switchToLogInBtn = document.getElementById('switch-to-log-in-btn');
 
-var signUpForm = document.getElementById("sign-up-form");
-var logInForm = document.getElementById("log-in-form");
+var signUpForm = document.getElementById('sign-up-form');
+var logInForm = document.getElementById('log-in-form');
 
-var returnBtn = document.querySelectorAll(".auth__ctrl-back");
- 
-signUpBtn.onclick = function() {
-  modal.style.display = "flex";
-  signUpForm.style.display = "block";
-  logInForm.style.display = "none";
-}
+var returnBtn = document.querySelectorAll('.auth__ctrl-back');
 
-logInBtn.onclick = function() {
-  modal.style.display = "flex";
-  signUpForm.style.display = "none";
-  logInForm.style.display = "block";
-}
+signUpBtn.onclick = function () {
+  modal.style.display = 'flex';
+  signUpForm.style.display = 'block';
+  logInForm.style.display = 'none';
+};
 
-switchToSignUpBtn.onclick = function() {
-  signUpForm.style.display = "block";
-  logInForm.style.display = "none";
-}
+logInBtn.onclick = function () {
+  modal.style.display = 'flex';
+  signUpForm.style.display = 'none';
+  logInForm.style.display = 'block';
+};
 
-switchToLogInBtn.onclick = function() {
-  signUpForm.style.display = "none";
-  logInForm.style.display = "block";
-}
+switchToSignUpBtn.onclick = function () {
+  signUpForm.style.display = 'block';
+  logInForm.style.display = 'none';
+};
 
-returnBtn[0].onclick = function() {
-  modal.style.display = "none";
-}
+switchToLogInBtn.onclick = function () {
+  signUpForm.style.display = 'none';
+  logInForm.style.display = 'block';
+};
 
-returnBtn[1].onclick = function() {
-  modal.style.display = "none";
-}
+returnBtn[0].onclick = function () {
+  modal.style.display = 'none';
+};
+
+returnBtn[1].onclick = function () {
+  modal.style.display = 'none';
+};
 
 // When the user clicks anywhere outside of the modal, close it
-close.onclick = function(event) {
+close.onclick = function (event) {
   if (event.target == close) {
-    modal.style.display = "none";
+    modal.style.display = 'none';
   }
-}
-
+};
